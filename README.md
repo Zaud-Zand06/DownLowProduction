7acb0fa4efb0e8223636bcac18552fff72e60106
Alex <Alex3388@live.ca>
2024-05-24 03:54:12 -0700

    added working hamburger menu and styling

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 Rebuild of the Downlow Chicken site for the DownLow company.
 
-Built with a React and VanillaJs.
-
-Using Vite, Bun and a few additional libraries.
+Built with a React and VanillaJs, using Vite, Bun and a few additional libraries.
 
 Libraries:
 react-responsive-carousel
 react-scroll-parallax
+@mui/material
+@emotion/react
+@emotion/styled
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import "./App.css";
+import { useState } from "react";
 import HeroCarousel from "./components/HeroCarousel";
 import { Parallax } from "react-scroll-parallax";
+import { Drawer } from "@mui/material";
 
 function App() {
+  const [displayMenu, setDisplayMenu] = useState(false);
+  const [hamburgerMenu, setHamburgerMenu] = useState(false);
+
+  function toggleHambugerMenu() {
+    setHamburgerMenu(!hamburgerMenu);
+  }
+
   return (
     <>
       <div className="header">
@@ -10,63 +19,107 @@ function App() {
           <h1>Down Low</h1>
           <h1 id="redLogo">Chicken</h1>
         </div>
-        <a href="" id="hambugerButton">
-          <h1>=</h1>
-        </a>
+        {/* rework to a seperate module */}
+        <>
+          <a
+            id="hamburgerButton"
+            onClick={() => {
+              toggleHambugerMenu();
+            }}
+          >
+            =
+          </a>
+
+          <Drawer
+            anchor="bottom"
+            elevation={0}
+            PaperProps={{ sx: { backgroundColor: "#00000000" } }}
+            open={hamburgerMenu}
+            onClose={() => {
+              setHamburgerMenu(false);
+            }}
+          >
+            <div className="hamburgerMenuButtons">
+              <a href="" target="_blank">
+                East Van Ordering
+              </a>
+              <a href="" target="_blank">
+                UBC Ordering
+              </a>
+              <a href="">Menu</a>
+              <a href="">Catering</a>
+            </div>
+          </Drawer>
+        </>
       </div>
 
-      <HeroCarousel />
-
-      <div className="scrollContainer">
-        <div className="horizontalScrollingItems">
-          <div className="horizontalScrollingItemsItems">
-            We Now Do Catering! We Now Do Catering! We Now Do Catering! We Now
-            Do Catering! We Now Do Catering! We Now Do Catering!
+      {displayMenu == true && (
+        <>
+          <div className="menu">
+            <a href="">Home</a>
+            <a href="">Menu</a>
+            <a href="">About Us</a>
+            <a href="">Contact</a>
           </div>
-          <div className="horizontalScrollingItemsItems">
-            We Now Do Catering! We Now Do Catering! We Now Do Catering! We Now
-            Do Catering! We Now Do Catering! We Now Do Catering!
-          </div>
-        </div>
-      </div>
+        </>
+      )}
 
-      <div className="parallaxContainer">
-        <div className="aboutUs">
-          <Parallax speed={-40} style={{ position: "relative", zIndex: 3 }}>
-            <div>info about us</div>
+      {displayMenu == false && (
+        <>
+          <HeroCarousel />
+
+          <div className="scrollContainer">
+            <div className="horizontalScrollingItems">
+              <div className="horizontalScrollingItemsItems">
+                We Now Do Catering! We Now Do Catering! We Now Do Catering! We
+                Now Do Catering! We Now Do Catering! We Now Do Catering!
+              </div>
+              <div className="horizontalScrollingItemsItems">
+                We Now Do Catering! We Now Do Catering! We Now Do Catering! We
+                Now Do Catering! We Now Do Catering! We Now Do Catering!
+              </div>
+            </div>
+          </div>
+
+          <div className="parallaxContainer">
+            <div className="aboutUs">
+              <Parallax speed={-40} style={{ position: "relative", zIndex: 3 }}>
+                <div>info about us</div>
+              </Parallax>
+              <Parallax speed={20} style={{ position: "relative", zIndex: 1 }}>
+                <video
+                  autoPlay={true}
+                  muted={true}
+                  loop={true}
+                  width={450}
+                  src="/src/assets/downLowPromoVid1.mp4"
+                ></video>
+              </Parallax>
+            </div>
+            <div className="locationContact">
+              <Parallax speed={-20} style={{ position: "relative", zIndex: 3 }}>
+                <div>info about the contacts and locations</div>
+              </Parallax>
+              <Parallax speed={40} style={{ position: "relative", zIndex: 1 }}>
+                <video
+                  autoPlay={true}
+                  muted={true}
+                  loop={true}
+                  width={450}
+                  src="/src/assets/downLowPromoVid2.mp4"
+                ></video>
+              </Parallax>
+            </div>
+          </div>
+          <Parallax speed={10} style={{ position: "relative", zIndex: 5 }}>
+            <iframe
+              className="mapEmbed"
+              src="https://storage.googleapis.com/maps-solutions-umrctwt8ee/locator-plus/jrgd/locator-plus.html"
+              loading="lazy"
+            ></iframe>
           </Parallax>
-          <Parallax speed={20} style={{ position: "relative", zIndex: 1 }}>
-            <video
-              autoPlay={true}
-              muted={true}
-              loop={true}
-              width={450}
-              src="/src/assets/downLowPromoVid1.mp4"
-            ></video>
-          </Parallax>
-        </div>
-        <div className="locationContact">
-          <Parallax speed={-20} style={{ position: "relative", zIndex: 3 }}>
-            <div>info about the contacts and locations</div>
-          </Parallax>
-          <Parallax speed={40} style={{ position: "relative", zIndex: 1 }}>
-            <video
-              autoPlay={true}
-              muted={true}
-              loop={true}
-              width={450}
-              src="/src/assets/downLowPromoVid2.mp4"
-            ></video>
-          </Parallax>
-        </div>
-      </div>
-      <Parallax speed={10} style={{ position: "relative", zIndex: 5 }}>
-        <iframe
-          className="mapEmbed"
-          src="https://storage.googleapis.com/maps-solutions-umrctwt8ee/locator-plus/jrgd/locator-plus.html"
-          loading="lazy"
-        ></iframe>
-      </Parallax>
+        </>
+      )}
     </>
   );
 }
