194cdd9049fa9e818d4b9ab121cbf504d843d7ae
Alex <Alex3388@live.ca>
2024-06-20 00:25:33 -0700

    changed background image size to space out logo, replaced logo with textless version, changed spacing of parallax section to fill empty space

--- src/components/ParallaxSection.jsx
+++ src/components/ParallaxSection.jsx
@@ -6,33 +6,33 @@ import turkeyDLF from "../assets/TurkeyDLF.jpg";
 function ParallaxSection() {
   return (
     <div className="parallaxContainer">
-      <Parallax speed={30} style={{ position: "relative", zIndex: 3 }}>
-        <p className="parallaxText">
+      <Parallax className="parallaxText" speed={30}>
+        <p>
           Since our inception, Down Low Chicken has been commited to providing
           the best Nashville style fried chicken Vancouver has to offer!
         </p>
       </Parallax>
-      <Parallax speed={-10} style={{ position: "relative" }}>
-        <img className="parallaxImage" src={agrodulce4} />
+      <Parallax className="parallaxImage" speed={-10}>
+        <img src={agrodulce4} />
       </Parallax>
-      <Parallax speed={30} style={{ position: "relative", zIndex: 3 }}>
-        <p className="parallaxText2">
+      <Parallax className="parallaxText2" speed={30}>
+        <p>
           From our juicy fried chicken to our custom dessert cups, we only
           source the highest quality and local ingredients for our recipes.
         </p>
       </Parallax>
-      <Parallax speed={20} style={{ position: "relative", zIndex: 3 }}>
-        <p className="parallaxText3">
+      <Parallax className="parallaxText3" speed={20}>
+        <p>
           Our fresh BC chicken is provided to us by Legends Haul, a halal
           certified supplier that shares our commitment to quality and our
           attention to detail.
         </p>
       </Parallax>
-      <Parallax speed={-10} style={{ position: "relative" }}>
-        <img className="parallaxImage2" src={turkeyDLF} />
+      <Parallax className="parallaxImage2" speed={-10}>
+        <img src={turkeyDLF} />
       </Parallax>
-      <Parallax speed={30} style={{ position: "relative", zIndex: 3 }}>
-        <p className="parallaxText3">
+      <Parallax className="parallaxText4" speed={30}>
+        <p>
           And yes, we can accomodate for all dietary restrictions! Come in and
           ask us what we can do for you!
         </p>
