ebebf99e985bce65cd5ac0cd7bbcb48878249265
Alex <alex3388@live.ca>
2025-01-10 12:03:18 -0800

    change alt text for herocarousel

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -7,31 +7,40 @@ import dessertCup from "../assets/DessertCup1.jpg";
 import jerry from "../assets/jerry1.jpg";
 import sidesImage from "../assets/sides.jpg";
 
-// Slides should be an array of objects with with an image, description, and title
+// Slides should be an array of objects with with an image, description, title and alt text.
+// you can have as many slides as you want, but try and limit it to less than 10 just so its not too crazy
 const slides = [
   {
     title: "The Agrodulce!",
     image: agroDulce1,
     description:
       "Our newest sando is a sweet and tangy blend of classic italian flavours! Basil mayo, basil slaw, our updated pizza dust, and our lovingly prepared Agrodulce, topped with a heavy cheese dusting.",
+    altText:
+      "A fried chicken sandwhich with basil mayo, basil slaw, pizza dust, and agrodulce. Topped with a heavy cheese dusting.",
   },
   {
     title: "Beans are back!",
     image: sidesImage,
     description:
-      "You asked, we delivered! Our bacon baked beans are back for the winter season. Make sure to get them before they're gone!",
+      "You asked, we delivered! Our Bacon Baked Beans and creamy Mac and Cheese are back for the winter season. Make sure to get them before they're gone!",
+    altText:
+      "A side of bacon baked beans, topped with cheese in a to go container, next to it is a side of creamy looking mac and cheese.",
   },
   {
     title: "Leave room for dessert!",
     image: dessertCup,
     description:
       "Made fresh by our lovely friends at Vennies. We're always offering Lemon Curd Cake and Chocolate Mousse Cake, with a rotating selection of speciality cups for you to try!",
+    altText:
+      "A dessert cup filled with strawberry cake, surrounded by a bunch of sliced strawberries.",
   },
   {
     title: "Been here before?",
     image: jerry,
     description:
       "Try customizing your favourite sando with different toppings like bacon, cheese, or even a fried egg!",
+    altText:
+      "A massive fried chicken sandwhich, made with two butterflied legs, topped with melted cheese and bacon.",
   },
 ];
 
@@ -57,7 +66,7 @@ function HeroCarousel() {
     return slides.map((slide, index) => {
       return (
         <div key={index} className="slideDiv">
-          <img src={slide.image} alt={slide.description} />
+          <img src={slide.image} alt={slide.altText} />
           <div className="slideText">
             <h2>{slide.title}</h2>
             <p>{slide.description}</p>
