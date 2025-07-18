ca5b612f7c4d8863d92e9d35b73f6ccf0e66369c
Alex <alex3388@live.ca>
2025-07-18 14:16:03 -0700

    placeholder events calendar

--- src/components/UpcomingEvents.jsx
+++ src/components/UpcomingEvents.jsx
@@ -4,14 +4,24 @@ import Footer from "./Footer.jsx";
 
 // add google calendar api to pull events from dlbrand events calendar
 
+const calendarClientID =
+  "415435788576-8h02j9udhgtl1of5q9nor1iqnckdnet8.apps.googleusercontent.com";
 const calendarAPIKey = import.meta.env.VITE_CALENDAR_API;
+const upcomingEventsList = [];
 
 export default function UpcomingEvents() {
   return (
     <>
       <Header />
       <div className="backgroundImage"></div>
-      <section id="eventsSection"></section>
+      <section id="eventsSection">
+        <h1>Come find us around the city at these events!</h1>
+        <iframe
+          src="https://calendar.google.com/calendar/embed?src=7beb9a770b57e614aeb5ddf51007975d77a3d739aed5ea639b21a65602a20af6%40group.calendar.google.com&ctz=America%2FVancouver"
+          width="800"
+          height="600"
+        ></iframe>
+      </section>
       <Footer />
     </>
   );
