8e89c3def83ff41e340025b22e939c4a2bfa4bbf
Alex <alex3388@live.ca>
2025-07-18 14:59:54 -0700

    add routing to calendar

--- src/components/ScrollingAlert.jsx
+++ src/components/ScrollingAlert.jsx
@@ -1,4 +1,5 @@
 import "./component_css/scrollingAlert.css";
+import { Link } from "react-router-dom";
 
 function ScrollingAlert() {
   const text = `Click Here to see what events we have coming up!`;
@@ -9,10 +10,10 @@ function ScrollingAlert() {
     <div className="scrollContainer">
       <div className="horizontalScrollingItems">
         <div className="horizontalScrollingItemsItems">
-          <a target="_blank">{repeatedText}</a>
+          <Link to="/events">{repeatedText}</Link>
         </div>
         <div className="horizontalScrollingItemsItems">
-          <a target="_blank">{repeatedText}</a>
+          <Link to="/events">{repeatedText}</Link>
         </div>
       </div>
     </div>
