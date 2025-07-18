7c6c57364d64c7e2d693ec94702ef2bfa48080b8
Alex <alex3388@live.ca>
2025-07-18 15:25:31 -0700

    add responsive sizing to placeholder calendar

--- src/components/UpcomingEvents.jsx
+++ src/components/UpcomingEvents.jsx
@@ -1,4 +1,5 @@
 import "./component_css/upcomingEvents.css";
+import { useEffect, useState } from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 
@@ -10,6 +11,23 @@ const calendarAPIKey = import.meta.env.VITE_CALENDAR_API;
 const upcomingEventsList = [];
 
 export default function UpcomingEvents() {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
       <Header />
@@ -18,8 +36,8 @@ export default function UpcomingEvents() {
         <h1>Come find us around the city at these events!</h1>
         <iframe
           src="https://calendar.google.com/calendar/embed?src=7beb9a770b57e614aeb5ddf51007975d77a3d739aed5ea639b21a65602a20af6%40group.calendar.google.com&ctz=America%2FVancouver"
-          width="800"
-          height="600"
+          width={windowSize.width * 0.75}
+          height={windowSize.height * 0.6}
         ></iframe>
       </section>
       <Footer />
