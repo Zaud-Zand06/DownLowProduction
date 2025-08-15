acaccf3c5783e9005f443f6606ba01714d5300d7
Alex <alex3388@live.ca>
2025-08-15 15:14:08 -0700

    changer scrolling alert to link to dl kits info

--- src/components/ScrollingAlert.jsx
+++ src/components/ScrollingAlert.jsx
@@ -4,7 +4,7 @@ const instaLink =
   "https://www.instagram.com/dlchicken?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==";
 
 function ScrollingAlert() {
-  const text = `Click Here to Check out our Insta!`;
+  const text = `Click Here For Information About Kits!`;
   const repetitions = 10;
   const repeatedText = Array(repetitions).fill(text).join(" ");
 
@@ -12,14 +12,10 @@ function ScrollingAlert() {
     <div className="scrollContainer">
       <div className="horizontalScrollingItems">
         <div className="horizontalScrollingItemsItems">
-          <a href={instaLink} target="_blank">
-            {repeatedText}
-          </a>
+          <Link to={"/kits"}>{repeatedText}</Link>
         </div>
         <div className="horizontalScrollingItemsItems">
-          <a href={instaLink} target="_blank">
-            {repeatedText}
-          </a>
+          <Link to={"/kits"}>{repeatedText}</Link>
         </div>
       </div>
     </div>
