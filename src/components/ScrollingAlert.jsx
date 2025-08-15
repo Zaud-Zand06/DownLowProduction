d4368ffabc17ada43c4e46d25dbaa4395ee637a4
Alex <alex3388@live.ca>
2025-08-15 12:32:46 -0700

    swap scrolling text from dl/events to dl instagram

--- src/components/ScrollingAlert.jsx
+++ src/components/ScrollingAlert.jsx
@@ -1,8 +1,10 @@
 import "./component_css/scrollingAlert.css";
 import { Link } from "react-router-dom";
+const instaLink =
+  "https://www.instagram.com/dlchicken?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==";
 
 function ScrollingAlert() {
-  const text = `Click Here to see what events we have coming up!`;
+  const text = `Click Here to Check out our Insta!`;
   const repetitions = 10;
   const repeatedText = Array(repetitions).fill(text).join(" ");
 
@@ -10,10 +12,14 @@ function ScrollingAlert() {
     <div className="scrollContainer">
       <div className="horizontalScrollingItems">
         <div className="horizontalScrollingItemsItems">
-          <Link to="/events">{repeatedText}</Link>
+          <a href={instaLink} target="_blank">
+            {repeatedText}
+          </a>
         </div>
         <div className="horizontalScrollingItemsItems">
-          <Link to="/events">{repeatedText}</Link>
+          <a href={instaLink} target="_blank">
+            {repeatedText}
+          </a>
         </div>
       </div>
     </div>
