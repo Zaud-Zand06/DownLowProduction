3c465ceee4684ac161d49b08395d82e8f869a853
Alex <alex3388@live.ca>
2025-08-29 14:58:47 -0700

    change default behaviour of heroContent text background to be translucent and blurred for readability

--- src/components/HeroContent.jsx
+++ src/components/HeroContent.jsx
@@ -1,23 +1,26 @@
 import "./component_css/heroContent.css";
-import heroImage from "../assets/cowboySando1600.avif";
+import heroImage from "../assets/baldySando1600crop.avif";
 
 function HeroContent() {
   return (
     <section id="heroContent">
       <img
         src={heroImage}
-        alt="spiced chicken sando with sauteed peppers and onions"
+        alt="soy glazed fried chicken sando with shredded and fried spam, coleslaw and lemon mayo sauce"
         loading="eager"
         decoding="async"
       />
       <div id="heroContentText">
         <h1>What's New This Month?</h1>
         <p>
-          The Cowboy Sando! Made with our brand-new Cowboy Ranch and dusted with
-          our new Cowboy Spice dust, topped with our Signature Slaw and BnB
-          Pickles!
+          The Baldy Sando! Made with love and in Collaboration with{" "}
+          <a href="https://baldyloxvintage.com/" target="_blank">
+            BaldyLox Vintage
+          </a>{" "}
+          and featuring a crispy piece of Soy Glazed Chicken, shredded and fried
+          Halal Chicken Spam, our famous Coleslaw and a new Lemon-y Rookie Mayo!
           <br />
-          Only available for August so come get it while it's around!
+          Available from Sept 3, to the end of the month! Get it while you can!
         </p>
       </div>
     </section>
